82932e56787c46c6a6917fa305dccbb08a4f0b3a
SphinxScript <alu0101643137@ull.edu.es>
2024-10-07 22:39:26 +0000

    p4 successfully done

--- CyA/p4/bucles.cc
+++ CyA/p4/bucles.cc
@@ -17,6 +17,6 @@
 #include "bucles.h"
 
 std::ostream& operator<<(std::ostream& os, const Bucles& otro) {
-  os << "[Line " << otro.GetNum() << "]" << otro.GetBucle() << std::endl;
+  os << "[Line " << otro.GetNum() << "] " << otro.GetBucle() << std::endl;
   return os;
 }
