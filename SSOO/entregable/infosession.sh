f663ff868c3a43dbcbd22cf5f09e6cea5cebacb9
SphinxScript <alu0101643137@ull.edu.es>
2024-10-16 15:33:26 +0000

    started at class

---
+++ SSOO/entregable/infosession.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+show_sidzero=0
+show_help=0
+
+while [ -n "$1" ]; do
+  case $1 in
+    -h)
+      show_help=1
+      ;;
+    -z)
+      show_sidzero=1
+      ;;
+    -u)
+      shift
+      if [[ $1 == -* ]]; then
+        echo "A la opcioón -u le falta un argumento"
+        exit 1
+      fi
+      user=$1
+      ;;
+    *)
+      echo "ERROR. Opción desconocida: '$1'"
+      exit 1
+  esac
+done
+
+if [ show_help=1 ]; then
+  echo "ayuda"
+  exit 0
+fi
+
+processes=$(get_processes)
+if [ $show_sidzero=0] ; then
+  processes=$(filter_sidzero "processes")
+fi
+
+if [ -n $user ]; then
+  processes=$(filter_by_user "$processes" "$user")
+fi
