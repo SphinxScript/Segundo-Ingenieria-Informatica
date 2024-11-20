7c6c7ea072681f94cf2775a74d7aee0350435202
SphinxScript <alu0101643137@ull.edu.es>
2024-11-20 09:49:43 +0000

    p2

--- Estructura_de_computadores/practica2/ficheros_para_llevar_hechos/microc.v
+++ Estructura_de_computadores/practica2/ficheros_para_llevar_hechos/microc.v
@@ -1,5 +1,29 @@
 module microc(output wire [5:0] Opcode, output wire z, input wire clk, reset, s_inc, s_inm, we3, wez, input wire [2:0] Op);
 //Microcontrolador sin memoria de datos de un solo ciclo
+  wire[9:0] salidaMUX, salidaSUM, salidaPC;
+  wire[15:0] salidaMEM;
+  wire[7:0] RD1, RD2, WD3;
 
+
+  wire zeroALU;
+  wire [7:0] salidaALU;
+
+  sum sumador(salidaSUM, 10'b0000000001, salidaPC);
+
+  registro #(10) PC(salidaPC, clk, reset, salidaMUX);
+
+  mux2 #(10) multiplexor(salidaMUX, salidaMEM[9:0], salidaSUM, s_inc);
+
+  memprog memoria(salidaMEM, clk, salidaPC);
+
+  regfile registros(RD1, RD2, clk, we3, salidaMEM[11:08], salidaMEM[07:04], salidaMEM[03:00], WD3);
+
+  alu ALU(salidaALU, zeroALU, RD1, RD2, Op);
+
+  ffd flag(clk, reset, zeroALU, wez, z);
+
+  mux2 #(8) multiplexor8(WD3, salidaALU, salidaMEM[11:04], s_inm);
+
+  assign Opcode = salidaMEM[15:10];
 
 endmodule
--- Estructura_de_computadores/practica2/ficheros_para_llevar_hechos/microc_tb.v
+++ Estructura_de_computadores/practica2/ficheros_para_llevar_hechos/microc_tb.v
@@ -1,30 +1,81 @@
-`timescale 1 ns / 10 ps
+`timescale 1ns/1ps
+
 module microc_tb;
+    // declaracion de variables
+    reg clk, reset;
+    reg s_inc, s_inm, we3, wez;
+    reg [2:0] Op;
+    wire [5:0] Opcode;
+    wire z;
 
-// declaración de variables
-//...
+    // Instancia del módulo microc a testear
+    microc uut (Opcode, z, clk, reset, s_inc, s_inm, we3, wez, Op);
 
-// instanciación del camino de datos
-//...
+    // Reloj: 20 ns por ciclo (sube a los 10 ns, baja a los 20 ns)
+    initial
+    begin
+        $dumpfile("microc_tb.vcd");
+        $dumpvars;
+        clk = 0;
+        #10 clk = ~clk;
+    end
 
-// generación de reloj clk
-//...
+    // Procedimiento de inicialización
+    initial
+      begin
 
-// Reseteo y configuración de salidas del testbench
-initial
-begin
-  $dumpfile("microc_tb.vcd");
-  $dumpvars;
-  // ... señal de reset
-end
+        // Inicialización de señales
+        reset = 1; s_inc = 0; s_inm = 0; we3 = 0; wez = 0; Op = 3'b000;
 
-// Bloque simulación señales control por ciclo
-initial
-begin
-  // retardos y señales para ejecutar primera instrucción (ciclo 1)
-  // retardos y señales para ejecutar segunda instrucción (ciclo 2)
-  // ...
-  $finish;
-end
+        // Inicio del programa: Reset activo durante los primeros 5 ns
+        #5
+        reset = 1;
 
+        // Ciclo 1: `j Start`
+        #5 
+        s_inc = 0; we3 = 0; wez = 0; 
+        
+        #5
+        s_inc = 0;                   
+
+        // Ciclo 2: `nop`
+        #10
+        s_inc = 1;                   
+
+        // Ciclo 3: `li #0 R2`
+        #10
+        s_inm = 1; we3 = 1;          
+
+        // Ciclo 4: `li #2 R1`
+        #10
+        s_inm = 1; we3 = 1;          
+
+        // Ciclo 5: `li #4 R3`
+        #10
+        s_inm = 1; we3 = 1;          
+
+        // Ciclo 6: `li #1 R4`
+        #10
+        s_inm = 1; we3 = 1;          
+
+        // Ciclo 7: `add R2 R3 R2`
+        #10
+        s_inm = 0; we3 = 1; wez = 1; 
+
+        // Ciclo 8: `sub R1 R4 R1`
+        #10
+        s_inm = 0; we3 = 1; wez = 1; 
+
+        // Ciclo 9: `jnz Iter`
+        #10
+        if (z == 0) s_inc = 0; else s_inc = 1; 
+
+        // Ciclo 10-14: Repetir o finalizar (bucle `Fin`)
+        #10
+        s_inc = 1;                   
+
+        // Terminar simulación
+        #300
+        $finish; 
+    end
 endmodule
