4bac01d5c41920662d0d60b003b6ddc4f25d28c3
SphinxScript <alu0101643137@ull.edu.es>
2026-03-19 17:20:09 +0000

    DispersionFunctions have been implemented at dispersionfunction.h file

---
+++ AYEDA/prtc02/src/dispersionfunction.h
@@ -0,0 +1,54 @@
+#pragma once
+
+
+
+template<class Key>
+class DispersionFunction {
+ public:
+  ~DispersionFunction() {}
+  virtual unsigned operator() (const Key& key) const = 0;
+};
+
+
+
+template <class Key>
+class DfModule : public DispersionFunction<Key> {
+ public:
+  DfModule(unsigned tablesize) : tablesize_(tablesize) {}
+  unsigned operator() (const Key& key) const override {
+    return key % tablesize_;
+  }
+ private:
+  unsigned tablesize_;
+};
+
+
+template <class Key>
+class DfSum : public DispersionFunction<Key> {
+ public:
+  DfSum(unsigned tablesize) : tablesize_(tablesize) {}
+  unsigned operator() (const Key& key) const override {
+    long val = long(key);
+    unsigned sum = 0;
+    while (val > 0) {
+      sum += val % 10;
+      val /= 10;
+    }
+    return sum % tablesize_;
+  }
+ private:
+  unsigned tablesize_;
+};
+
+
+template <class Key>
+class DfRandom : public DispersionFunction<Key> {
+ public:
+  DfRandom(unsigned tablesize) : tablesize_(tablesize) {}
+  unsigned operator() (const Key& key) const override {
+    srand((long)key);
+    return rand() % tablesize_;
+  }
+ private:
+  unsigned tablesize_;
+};
